487330f909e79a62a794c68ac2313af5f5b8c11e
Ram Suresh <yramsuresh1993@gmail.com>
2025-11-23 20:51:49 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { Provider } from "react-redux";
-import "./App.css";
-import Posts from "./components/posts/Posts";
-import store from "./redux/store";
+import { Provider } from "react-redux"
+import "./App.css"
+import Posts from "./components/posts/Posts"
+import store from "./redux/store"
 function App() {
+  console.log("hello")
   return (
     <Provider store={store}>
       <div className="container py-3">
         <Posts />
       </div>
     </Provider>
-  );
+  )
 }
-export default App;
+export default App
